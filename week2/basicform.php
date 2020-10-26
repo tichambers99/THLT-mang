b3b658d6db38d437743d63e28f821aeb5260d873
tichambers99 <khanhkhanhars@gmail.com>
2020-10-26 22:06:14 +0700

    week2

---
+++ week2/basicform.php
@@ -0,0 +1,15 @@
+<HTML>
+    <HEAD>
+        <TITLE>A simple form</TITLE>
+    </HEAD>
+    <BODY>
+        <FORM ACTION="http://fit.hut.edu.vn/~trangntt/courses/vp" METHOD=POST>
+            Click submit to start our initial PHP profram
+            <BR>
+            <input type="text" name="Name">
+            <BR>
+            <input TYPE="submit" VALUE="Click to submit">
+            <input type="RESET" VALUE="Erase and restart">
+        </FORM>
+    </BODY>
+</HTML>
---
+++ week2/registorForm.php
@@ -0,0 +1,19 @@
+<HTML>
+    <HEAD>
+        <TITLE>A simple register form</TITLE>
+    </HEAD>
+    <BODY>
+        <FORM ACTION="http://fit.hut.edu.vn/~trangntt/courses/vp" METHOD=POST>
+            <h2>Let register account!!!</h2>
+            <BR>
+            <input type="text" name="Name" placeholder="Enter your name">
+            <BR>
+            <input TYPE="email" name="email" placeholder="Username">
+            <BR>
+            <input type="password" name="password" placeholder="Password">
+            <input type="password" name="confirm-password" placeholder="Confirm password">
+            <BR>
+            <input TYPE="submit" VALUE="Click to register">
+        </FORM>
+    </BODY>
+</HTML>
